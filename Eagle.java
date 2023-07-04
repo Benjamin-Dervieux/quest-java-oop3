01d86c18167937b7d810698b7635887b975612e0
[dervieux.ben@gmail.com](mailto:dervieux.ben@gmail.com) <dervieux.ben@gmail.com>
2023-07-04 15:36:02 +0200

    quest 8 java

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,48 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+@Override
+public void takeOff() {
+    flying = true;
+    System.out.println(getName() + " takes off in the sky.");
 }
+
+@Override
+public int ascend(int meters) {
+    if (flying) {
+        altitude += meters;
+        System.out.println(getName() + " flies upward, altitude :  " + altitude + " meters.");
+    }
+    return altitude;
+}
+
+@Override
+public int descend(int meters) {
+    if (flying) {
+        altitude -= meters;
+        System.out.println(getName() + " flies downward, altitude :  " + altitude + " meters.");
+    }
+    return altitude;
+}
+
+@Override
+public int glide() {
+    if (flying) {
+        System.out.println( "glides into the air.");
+    }
+    return altitude;
+}
+
+@Override
+public void land() {
+    if (altitude == 0) {
+        flying = false;
+        System.out.println(getName() + " lands on the ground.");
+    } else {
+        System.out.println(getName() + " is too high, it can't land.");
+    }
+}
+
+}
+
